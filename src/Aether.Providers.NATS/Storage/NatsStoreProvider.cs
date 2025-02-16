83cf492efa94815b9370b648c63c10208df983e8
Rick <rick@rickdot.net>
2025-02-16 15:15:59 -0600

    v0.3.6 - allow users to set a global default NatsJSOpts
    Not ideal, but gets us down the road

--- src/Aether.Providers.NATS/Storage/NatsStoreProvider.cs
+++ src/Aether.Providers.NATS/Storage/NatsStoreProvider.cs
@@ -1,6 +1,7 @@
 using Aether.Abstractions.Hosting;
 using Aether.Abstractions.Storage;
 using NATS.Client.Core;
+using NATS.Client.JetStream;
 using NATS.Client.KeyValueStore;
 using NATS.Net;
 
@@ -10,9 +11,21 @@ public sealed class NatsStoreProvider : IStoreProvider
 {
     private readonly INatsKVContext kvContext;
 
-    public NatsStoreProvider(INatsConnection nats)
+    public NatsStoreProvider(INatsConnection nats, NatsJSOpts? defaultJsOpts)
     {
-        kvContext = nats.CreateKeyValueStoreContext();
+        defaultJsOpts ??= new NatsJSOpts(nats.Opts)
+        {
+            DefaultConsumeOpts = new NatsJSConsumeOpts()
+            {
+                // TODO: 1 MB as int
+                MaxBytes = 1 * 1024 * 1024,
+
+                MaxMsgs = 1_000
+            }
+        };
+        
+        var jsContext = new NatsJSContext(nats, defaultJsOpts with {});
+        kvContext = jsContext.CreateKeyValueStoreContext();
     }
 
     public async Task<IStore> CreateStore(StorageRegistration registration,
