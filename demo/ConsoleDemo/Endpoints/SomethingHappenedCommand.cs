ffe29d0146a8fcd839ca9fff13bbec989c98aa19
Rick <rick@rickdot.net>
2025-02-17 17:50:33 -0600

    Remove old demo project and init new one

--- demo/ConsoleDemo/Endpoints/SomethingHappenedCommand.cs
+++
@@ -1,5 +0,0 @@
-using Aether.Abstractions.Messaging;
-
-namespace ConsoleDemo.Endpoints;
-
-public record SomethingHappenedCommand(string Message) : ICommand;
---
+++ src/Scenarios/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("New Scenarios Coming Soon!");
